44a45ea2a0109052069423c223ac06460b910992
lbaitemple <lbai@temple.edu>
2020-10-01 17:00:51 -0400

    udpate
    update

--- lab5a/lvl_sen_sr_latch.sv
+++ lab5a/lvl_sen_sr_latch.sv
@@ -4,11 +4,9 @@
 module lvl_sen_sr_latch (output logic q, input logic s, input logic r,
 	input logic c);
 
-	logic s1, r1;	// outputs of the AND gates
 
-	assign s1 = s & c;
-	assign r1 = r & c;
-
-	sr_latch u_sr (.qa(), .qb(q), .s(s1), .r(r1));
+	// add output from and gates
+	
+	// add init code here
 
 endmodule
