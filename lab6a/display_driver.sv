4aa274bda1ff1c0d42f52fe08a679f77ad619ab6
lbaitemple <lbai@temple.edu>
2020-10-22 16:20:27 -0400

    Update display_driver.sv

--- lab6a/display_driver.sv
+++ lab6a/display_driver.sv
@@ -9,18 +9,9 @@ module display_driver( output logic [6:0] cathode, output logic [3:0] anode,
 	logic [3:0] bcd_in; // output of mux, input to seven segment decoder
 
 	// digit multiplexer
-	always_comb begin
-		case (anode_sel)
-			2'b00: bcd_in = digit0;
-			2'b01: bcd_in = digit1;
-			2'b10: bcd_in = digit2;
-			default: bcd_in = digit3;
-		endcase
-	end
+
 
 	// instantiate the modules
-	svn_seg_decoder u_sd (.seg_out(cathode), .bcd_in, .display_on);
-	anode_decoder u_ad (.anode, .switch_in(anode_sel));
-	d_counter u_dc (.ms_bits(anode_sel), .clk, .rst);
+
 
 endmodule
