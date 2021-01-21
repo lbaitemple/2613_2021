0fb25bfe0bff148b2c3814fe6483f4cfa0428948
lbaitemple <lbai@temple.edu>
2021-01-20 19:27:15 -0500

    Update create_sshd.sh

--- create_sshd.sh
+++ create_sshd.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [ $# -lt 1 ]; 
+   then 
+   printf "\nNot enough arguments - You should use the following command \n\n"
+   printf "./create_sshd.sh tuaccessnet@ece-000.eng.temple.edu \n\n"
+   exit 0 
+fi 
+
 mkdir -p ./ssh
 ssh-keygen -t rsa -b 4096 -C $1 -f ./ssh/id_rsa
 cat ./ssh/id_rsa.pub | ssh $1 "mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
