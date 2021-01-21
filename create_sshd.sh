4eb69a1a8e1fc0995d90b6e31710928c330d55c7
lbaitemple <lbai@temple.edu>
2021-01-20 19:15:28 -0500

    Create create_sshd.sh

---
+++ create_sshd.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+ssh-keygen -t rsa -b 4096 -C $1 -f ~/ssh/id_rsa.pub
+cat ~/ssh/id_rsa.pub | ssh $1 "mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
