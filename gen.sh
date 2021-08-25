aa613ee6d1ed5402b9d69a857ac19e39253c56ae
lbaitemple <lbai@temple.edu>
2021-08-25 12:02:41 -0400

    Create gen.sh

---
+++ gen.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ 
+echo "Enter Your TUACCESSNET ID: "
+read accessnetid
+echo "Welcome ${accessnetid}!"
+
+
+
+ssh-keygen -q -t rsa -b 4096 -C "{accessnetid}@ece-000.eng.temple.edu"
+ssh-copy-id -f  ${accessnetid}@ece-000.eng.temple.edu
+
+
+mkdir ece2613
+echo "alias conn2613='sshfs {accessnetid}@ece-000.eng.temple.edu:/home/{accessnetid}/ece2613 /home/ubuntu/environment/ece2613'" >> ~/.bashrc
+
+sudo sh -c 'echo "{accessnetid}@ece-000.eng.temple.edu:/home/{accessnetid}/ece2613 /home/ubuntu/environment/ece2613  fuse.sshfs  defaults  0  0" >> /etc/fstab' 
