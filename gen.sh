9b51e3dd4961ca7b8601232b576620bc248712ef
lbaitemple <lbai@temple.edu>
2021-09-08 14:07:39 -0400

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -10,7 +10,7 @@ ssh-keygen -q -t rsa -b 4096 -C "${accessnetid}@ece-000.eng.temple.edu"
 ssh-copy-id -f  ${accessnetid}@ece-000.eng.temple.edu
 
 
-mkdir ece2613
+mkdir ~/environment/ece2613
 echo "alias conn2613='sshfs ${accessnetid}@ece-000.eng.temple.edu:/home/${accessnetid}/ece2613 /home/ubuntu/environment/ece2613'" >> ~/.bashrc
 
 sudo sh -c 'echo "${accessnetid}@ece-000.eng.temple.edu:/home/${accessnetid}/ece2613 /home/ubuntu/environment/ece2613  fuse.sshfs  defaults  0  0" >> /etc/fstab' 
