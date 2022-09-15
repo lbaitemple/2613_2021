7683176c63d79ceb0743cb1a016d830eb627bafa
lbaitemple <lbai@temple.edu>
2022-09-15 14:19:47 -0400

    Update updateos.sh

--- updateos.sh
+++ updateos.sh
@@ -2,3 +2,8 @@ mkdir -p ~/.vscode-server/data/Machine
 cp settings.json ~/.vscode-server/data/Machine
 rm -rf ~/.git
 rm -rf .git
+
+for i in lab1 lab2 lab3 lab4 lab5 lab5a lab6 lab6a lab7a
+do
+   cp ./intel_extra_files_2020f/$i/* $i/
+done
