0c08b3852ec735f1af2aff6ebdc75885094a9993
lbaitemple <lbai@temple.edu>
2023-01-25 14:45:08 -0500

    Update updateos.sh

--- updateos.sh
+++ updateos.sh
@@ -1,3 +1,7 @@
+echo "umask 027">> ~/.bash_profile
+echo "export ECE2612=/data/courses/ece_2612">> ~/.bash_profile
+echo "PATH=$PATH:$ECE2612/bin">> ~/.bash_profile
+
 mkdir -p ~/.vscode-server/data/Machine
 cp settings.json ~/.vscode-server/data/Machine
 rm -rf ~/.git
