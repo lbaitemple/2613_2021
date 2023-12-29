92a10e5341a581e02aaff1dfdb819ee657efa8b9
Li Bai <lbai@temple.edu>
2023-12-29 09:39:29 -0700

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -9,16 +9,20 @@ bash ./install-desktop.sh
 wget https://raw.githubusercontent.com/aws-samples/robotics-boilerplate/main/install-dcv.sh
 bash ./install-dcv.sh
 
- aws s3 cp s3://intellb/max10-22.1std.2.922.qdz .
- aws s3 cp s3://intellb/QuartusLiteSetup-22.1std.2.922-linux.run .
+wget https://github.com/lbaitemple/ece2613/releases/download/v1.0.0/max10-22.1std.2.922.qdz
+wget https://github.com/lbaitemple/ece2613/releases/download/v1.0.0/ModelSimSetup-20.1.1.720-linux.run
+wget https://github.com/lbaitemple/ece2613/releases/download/v1.0.0/QuartusLiteSetup-22.1std.2.922-linux.run
  chmod +x QuartusLiteSetup-22.1std.2.922-linux.run 
 ./QuartusLiteSetup-22.1std.2.922-linux.run --mode unattended --accept_eula 1 
- aws s3 cp s3://intellb/ModelSimSetup-20.1.1.720-linux.run .
+# aws s3 cp s3://intellb/max10-22.1std.2.922.qdz .
+# aws s3 cp s3://intellb/QuartusLiteSetup-22.1std.2.922-linux.run .
+# aws s3 cp s3://intellb/ModelSimSetup-20.1.1.720-linux.run .
  chmod +x ModelSimSetup-20.1.1.720-linux.run 
 ./ModelSimSetup-20.1.1.720-linux.run --mode unattended --accept_eula 1 
 #sudo  apt-get install libxft2 libxft2:i386 libxrender1:i386 libxtst6:i386 libxi6:i386 -y
 # aws s3 cp s3://intellb/backdoor.zip .
-gdown --id 1ESAcilkTnWTU6mUQ7irp1BWktr8h110n --output  backdoor.zip
+#gdown --id 1ESAcilkTnWTU6mUQ7irp1BWktr8h110n --output  backdoor.zip
+wget https://github.com/lbaitemple/ece2613/releases/download/v1.0.0/backdoor.zip
 unzip backdoor.zip 
 echo "export PATH='$PATH:/home/ubuntu/intelFPGA_lite/22.1std/quartus/bin:/home/ubuntu/environment/backdoor'" > ~/.bashrc
 
