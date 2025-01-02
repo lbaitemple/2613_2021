b64bed1023e48892979229ac4cf87032e123f434
Li Bai <lbai@temple.edu>
2025-01-02 16:14:35 -0500

    Update memory_display.sv

--- lab8/memory_display.sv
+++ lab8/memory_display.sv
@@ -3,7 +3,7 @@
 // 
 
 module memory_display (output logic [6:0] cathode, output logic [3:0] anode,
-	input logic rst, input logic clk, input logic [1:0] anode_sel,
+	input logic rst, input logic clk, 
 	output logic [3:0] rs_data, output logic [3:0] ru_data,
 	input logic w_ena, input logic [3:0] w_data, input logic [9:0] sw);
 
