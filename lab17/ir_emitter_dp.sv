cd6ee0f4fe6409f7f2ee69eeb8192cc656ef3243
Li Bai <lbai@temple.edu>
2025-01-05 15:42:42 -0500

    Update ir_emitter_dp.sv

--- lab17/ir_emitter_dp.sv
+++ lab17/ir_emitter_dp.sv
@@ -3,10 +3,8 @@
 //  
 `timescale 1ns/1ps
 
-module ir_emitter_dp (output logic emitter_out, output logic tc_modulator, input lo
-gic clk,
-	input logic rst, input logic ena, input [1:0] mod_sel, input logic sw_modul
-ator);
+module ir_emitter_dp (output logic emitter_out, output logic tc_modulator, input logic clk,
+	input logic rst, input logic ena, input [1:0] mod_sel, input logic sw_modulator);
 
 	// enter your code here
 
