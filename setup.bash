f0479d220c1d1bd529a2b0f9b280c3a874209b40
Li Bai <lbai@temple.edu>
2025-01-05 14:00:09 -0500

    Add files via upload

--- setup.bash
+++ setup.bash
@@ -15,19 +15,19 @@ wget https://github.com/lbaitemple/ece2613/releases/download/v2.0.0/max10-23.1st
 wget https://github.com/lbaitemple/ece2613/releases/download/v2.0.0/ModelSimSetup-20.1.1.720-linux.run
 wget https://github.com/lbaitemple/ece2613/releases/download/v2.0.0/QuartusLiteSetup-23.1std.0.991-linux.run
 
- chmod +x QuartusLiteSetup-22.1std.2.922-linux.run 
-./QuartusLiteSetup-22.1std.2.922-linux.run --mode unattended --accept_eula 1 
+ chmod +x QuartusLiteSetup-23.1std.0.991-linux.run
+./QuartusLiteSetup-23.1std.0.991-linux.run --mode unattended --accept_eula 1 
 # aws s3 cp s3://intellb/max10-23.1std.0.991.qdz .
 # aws s3 cp s3://intellb/QuartusLiteSetup-23.1std.0.991-linux.run .
 # aws s3 cp s3://intellb/ModelSimSetup-20.1.1.720-linux.run .
- chmod +x ModelSimSetup-20.1.1.720-linux.run 
+ chmod +x ModelSimSetup-20.1.1.720-linux.run
 ./ModelSimSetup-20.1.1.720-linux.run --mode unattended --accept_eula 1 
 #sudo  apt-get install libxft2 libxft2:i386 libxrender1:i386 libxtst6:i386 libxi6:i386 -y
 # aws s3 cp s3://intellb/backdoor.zip .
 #gdown --id 1ESAcilkTnWTU6mUQ7irp1BWktr8h110n --output  backdoor.zip
-wget https://github.com/lbaitemple/ece2613/releases/download/v1.0.0/backdoor.zip
+wget https://github.com/lbaitemple/ece2613/releases/download/v2.0.0/backdoor.zip
 unzip backdoor.zip 
-echo "export PATH='$PATH:/home/ubuntu/intelFPGA_lite/22.1std/quartus/bin:/home/ubuntu/environment/backdoor'" > ~/.bashrc
+echo "export PATH='$PATH:/home/ubuntu/intelFPGA_lite/23.1std/quartus/bin:/home/ubuntu/environment/backdoor'" > ~/.bashrc
 
 
 sudo dpkg --add-architecture i386 
