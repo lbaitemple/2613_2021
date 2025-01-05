2e00229840f08af71aa8d9bf32f5f427fd936523
Li Bai <lbai@temple.edu>
2025-01-05 13:20:23 -0500

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -11,13 +11,14 @@ sudo sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/nee
 wget https://raw.githubusercontent.com/aws-samples/robotics-boilerplate/main/install-dcv.sh
 cat ./install-dcv.sh | sudo -E bash -
 
-wget https://github.com/lbaitemple/ece2613/releases/download/v1.0.0/max10-22.1std.2.922.qdz
-wget https://github.com/lbaitemple/ece2613/releases/download/v1.0.0/ModelSimSetup-20.1.1.720-linux.run
-wget https://github.com/lbaitemple/ece2613/releases/download/v1.0.0/QuartusLiteSetup-22.1std.2.922-linux.run
+wget https://github.com/lbaitemple/ece2613/releases/download/v2.0.0/max10-23.1std.0.991.qdz
+wget https://github.com/lbaitemple/ece2613/releases/download/v2.0.0/ModelSimSetup-20.1.1.720-linux.run
+wget https://github.com/lbaitemple/ece2613/releases/download/v2.0.0/QuartusLiteSetup-23.1std.0.991-linux.run
+
  chmod +x QuartusLiteSetup-22.1std.2.922-linux.run 
 ./QuartusLiteSetup-22.1std.2.922-linux.run --mode unattended --accept_eula 1 
-# aws s3 cp s3://intellb/max10-22.1std.2.922.qdz .
-# aws s3 cp s3://intellb/QuartusLiteSetup-22.1std.2.922-linux.run .
+# aws s3 cp s3://intellb/max10-23.1std.0.991.qdz .
+# aws s3 cp s3://intellb/QuartusLiteSetup-23.1std.0.991-linux.run .
 # aws s3 cp s3://intellb/ModelSimSetup-20.1.1.720-linux.run .
  chmod +x ModelSimSetup-20.1.1.720-linux.run 
 ./ModelSimSetup-20.1.1.720-linux.run --mode unattended --accept_eula 1 
@@ -36,7 +37,7 @@ sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 gtkwave -y
 sudo apt-get install lib32z1 libxrender1:i386  libxft2 libxft2:i386  libxtst6:i386 libxi6:i386 -y
 #git clone -b spring2022 https://github.com/lbaitemple/ece2613 
 cp -r .c9 ~/environment
-sudo rm -rf *.run max10-22.1std.2.922.qdz backdoor.zip install*.sh nice-dcv-* 
+sudo rm -rf *.run max10-23.1std.0.991.qdz backdoor.zip install*.sh nice-dcv-* 
 #cd ece2613
 chmod +x updateos.sh
 ./updateos.sh
