85528c997cb0000bb73745f237c84096f3bc8c8b
Li Bai <lbai@temple.edu>
2025-01-31 13:48:41 -0500

    update

--- lab7c/lab7c_top_io_wrapper.tcl
+++
@@ -1,5 +0,0 @@
-set MODULE lab7a_top_io_wrapper
-set SOURCE_FILES "lab7a_top_io_wrapper.sv rotation_counter.sv up_down_bcd_counter.sv rc_fsm.sv rot_enc_rec.sv  up_down_count_dp.sv ../lab6a/display_driver.sv ../lab6a/d_counter.sv ../lab3/svn_seg_decoder.sv ../lab3/anode_decoder.sv"
-set CONSTRAINTS lab7a.xdc
-#
-source ../syn_generic.tcl
